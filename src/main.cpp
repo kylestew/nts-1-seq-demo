dd924c59900e31fa99e979992ab88145e066a02c
Kyle Stewart <kylestew@gmail.com>
2021-06-27 12:01:45 -0600

    Be careful with pin assignments or the chip is tough to recover

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,52 @@
 
 NTS1 nts1;
 
-// enum { sw_0 = 0, sw_1, sw_2, sw_3, sw_4, sw_5, sw_6, sw_7, sw_8, sw_9, sw_count };
-// const uint8_t sw_pins[sw_count] = {D34, D35, D36, D37, D38, D40, D41, D42, D46, D47};
+// -- HARDWARE UI pin definitiions and state ------------------------------------------
+enum { sw0 = 0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, swCount };
+const uint8_t swPins[swCount] = {PC8, PC6, PC5, PA12, PA11, PB11, PB2, PB1, PC4, PF5};
 
 // enum { sw_step0 = sw_0; };
 
-// enum { led_0 = 0, led_1, led_2, led_3, led_4, led_5, led_6, led_7, led_count };
-// const uint8_t led_pins[led_count] = {D16, D17, D18, D19, D22, D23, D24, D25};
+enum { led0 = 0, led1, led2, led3, led4, led5, led6, led7, ledCount };
+const uint8_t ledPins[ledCount] = {PC10, PC12, PF6, PF7, PA15, PB7, PC13, PC14};
+
+// -- SEQUENCER definitions and state -------------------------------------------------
+
+#define SEQ_LENGTH 8
+#define SEQ_TICKS_PER_STEP 100
+
+typedef struct {
+    uint32_t gates;
+} SeqState;
+
+SeqState seqState = {
+    .gates = 0xFF,  // 1 bit per step, all on
+};
+
+// -- UI Scan/Control -----------------------------------------------------------------
+
+void setStepLEDs(uint8_t mask) {
+    for (uint8_t i = led0; i < ledCount; ++i) {
+        digitalWrite(ledPins[i], (mask & (1U << i)) ? HIGH : LOW);
+    }
+}
+
+// -- SEQUENCER Runtime ---------------------------------------------------------------
+// -- HARDWARE Timer Setup ------------------------------------------------------------
+// -- MAIN ----------------------------------------------------------------------------
 
 void setup() {
     // init switches & LEDs
-    // for (uint8_t i = 0; i < sw_count; ++i) {
-    // pinMode(sw_pins[i], INPUT_PULLUP);
-    // }
-    // for (uint8_t i = 0; i < led_count; ++i) {
-    //     uint8_t led_pin = led_pins[i];
-    //     pinMode(led_pin, OUTPUT);
-    // }
-
-    pinMode(PC10, OUTPUT);
+    for (uint8_t i = 0; i < swCount; ++i) {
+        pinMode(swPins[i], INPUT_PULLUP);
+    }
+    for (uint8_t i = 0; i < ledCount; ++i) {
+        pinMode(ledPins[i], OUTPUT);
+    }
 
     nts1.init();
 
-    // TEMP: turn on all LEDs
-    // for (uint8_t i = led_0; i <= led_count; ++i) {
-    // digitalWrite(led_pins[i], HIGH);
-    // }
+    // setStepLEDs(seqState.gates);
 }
 
-void loop() {
-    nts1.idle();
-
-    digitalWrite(PC10, HIGH);
-    delay(300);
-
-    digitalWrite(PC10, LOW);
-    delay(800);
-}
+void loop() { nts1.idle(); }
